23ceeaa620b0105aa26b7b6bd4b4ea049a542cae
André Marconi Custódio <andre@Andres-Mac-mini.local>
2021-10-17 00:08:34 -0300

    Instalação pode ser feita a partir do diretório do projeto.

--- install.sh
+++ install.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
+DIR_ATUAL=${PWD##*/}
+DIR_REPO='preparacao-projetos'
+
+read -p "Você deseja configurar o projeto no diretório $DIR_ATUAL? [S]im ou [N]ão " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Ss]$ ]]
+  then
+  [[ "$0" = "$BASH_SOURCE" ]] && exit 0 || return 0
+fi
+
 echo "Criando estrutura do projeto de desenvolvimento."
 
-# echo "Clonando o repositório com os arquivos do projeto"
-# git clone https://github.com/andremarconi/preparacao-projetos.git
-# echo "Repositório clonado."
-# echo "Entrando no diretório."
-# cd preparacao-projetos
+    mv $DIR_REPO/.gitignore .gitignore
+    mv $DIR_REPO/.prettierrc .prettierrc
+    mv $DIR_REPO/.vscode .vscode
+    mv $DIR_REPO/.eslintrc.json .eslintrc.json
 
-echo "Removendo o diretório .git."
-rm -rf .git
-echo "Diretório removido."
-
-read -p "Qual projeto você vai instalar: [W]ebpack ou [S]CSS Sync? " -n 1 -r
+read -p "Qual projeto você vai instalar em $DIR_ATUAL: [W]ebpack ou [S]CSS Sync? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Ww]$ ]]
   then
@@ -20,21 +25,11 @@ if [[ $REPLY =~ ^[Ww]$ ]]
     echo "Criando os diretórios e arquivos para o Webpack."
     mkdir -p app/assets/{images,styles/{base,modules},scripts}
     touch app/assets/styles/styles.scss
-    echo "Diretórios app/assets/{images,styles/{base,modules},scripts e arquivos criados."
-
-    echo "Movendo arquivo app.js para o diretório scripts e index.html para o
-    diretório app."
-    mv app.js app/assets/scripts/app.js
-    mv index-webpack.html app/index.html
-    echo "Arquivos movidos."
-
-    echo "Renomeando arquivo package-webpack.json para package.json."
-    mv package-webpack.json package.json
-    echo "Arquivo renomeado."
-
-    echo "Removendo arquivos desnecessários."
-    rm package-proxy.json package-scss.json index-scss.html
-    echo "Arquivos removidos."
+    mv $DIR_REPO/app.js app/assets/scripts/app.js
+    mv $DIR_REPO/index-webpack.html app/index.html
+    mv $DIR_REPO/package-webpack.json package.json
+    mv $DIR_REPO/webpack.config.js webpack.config.js
+    echo "Arquivos criados."
 
     echo "Instalando pacotes npm."
     npm install webpack webpack-cli webpack-dev-server css-loader sass sass-loader style-loader --save-dev
@@ -44,6 +39,10 @@ if [[ $REPLY =~ ^[Ww]$ ]]
     echo "Building the project."
     npm run build
     echo "Projeto inicial construído."
+
+    echo "Removendo diretório $DIR_REPO"
+    rm -rf $DIR_REPO
+    echo "Diretório removido"
 
     echo "Estrutura do projeto usando Webpack foi criado."
     exit 0
@@ -59,25 +58,18 @@ fi
 
 echo "Criando os diretórios para o SyncBrowser e SCSS."
 mkdir -p {css,js,images,scss/{base,modules}}
-echo "Diretório scss{base,modules},css,js,images criados."
-
-echo "Criando os arquivos."
 touch scss/source.scss
-mv index-scss.html index.html
-echo "Arquivos index.html e scss/source.scss criados."
+mv $DIR_REPO/index-scss.html index.html
 
 read -p "O projeto SCSS Sync é sem proxy? [S]im ou [N]ão: " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Ss]$ ]]
   then
-    
+
     echo "Renomeando arquivo package-scss.json para package.json."
-    mv package-scss.json package.json
+    mv $DIR_REPO/package-scss.json package.json
     echo "Arquivo renomeado."
 
-    echo "Removendo arquivos desnecessários."
-    rm package-proxy.json package-webpack.json index-webpack.html app.js webpack.config.js
-    echo "Arquivos removidos."
   else
     if [[ ! $REPLY =~ ^[Nn]$ ]]
     then
@@ -88,19 +80,19 @@ fi
 if [[ $REPLY =~ ^[Nn]$ ]]
   then
     echo "Renomeando arquivo package-proxy.json para package.json."
-    mv package-proxy.json package.json
+    mv $DIR_REPO/package-proxy.json package.json
     echo "Arquivo renomeado."
 
-    echo "Removendo arquivos desnecessários."
-    rm package-scss.json package-webpack.json index-webpack.html app.js webpack.config.js
-    echo "Arquivos removidos."
 fi
     echo "Instalando pacotes npm."
     npm install browser-sync npm-run-all prettier sass --save-dev
     echo "Pacotes instalados."
 
+    echo "Removendo diretório $DIR_REPO"
+    rm -rf $DIR_REPO
+    echo "Diretório removido"
+
     echo "Estrutura do projeto usando SyncBrowser e SCSS criado."
     exit 0
-
 
 #END
